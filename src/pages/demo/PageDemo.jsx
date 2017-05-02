d065f09722f8d4da40810d3d95083b17be4b61ac
jianyuan.gjy <jianyuan.gjy@dtwave-inc.com>
2017-05-02 11:48:02 +0800

    init

---
+++ src/pages/demo/PageDemo.jsx
@@ -0,0 +1,45 @@
+import { Component } from 'reflux';
+
+import { withRouter } from 'react-router';
+
+
+import './PageDemo.less';
+import Actions from './actions';
+import Store from './store';
+import SearchWord from '../../components/search-word';
+import SearchData from '../../components/search-data';
+
+// 如果有`Action`和`Store`那么就使用`Reflux.Component`
+// 这样可以用`Reflux`管理全部的`state`
+// 在这里面改变`state`是不会生效的
+// 否则，还是使用`React.Component`
+class PageDemo extends Component {
+
+  constructor(props) {
+    super(props);
+    // 通过`this.store`建立和`Store`的联系
+    this.store = Store;
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    Actions.updateState({ workNo: e.target.value });
+  }
+
+  render() {
+    const { state: { content: { data }, workNo }, handleChange } = this;
+
+    return (
+      <div className="page-demo">
+        <input className="kuma-input" onChange={handleChange} placeholder="请输入员工工号" />
+        <SearchWord workNo={workNo} />
+        <SearchData data={data} />
+      </div>
+    );
+  }
+}
+
+
+export default withRouter(PageDemo); 
+
---
+++ src/pages/home/PageHome.jsx
@@ -0,0 +1,50 @@
+import { Component } from 'react';
+
+import { withRouter } from 'react-router';
+
+import { Table } from 'uxcore';
+
+import './PageHome.less';
+import { URLS } from '../../app/variables';
+
+// 如果有`Action`和`Store`那么就使用`Reflux.Component`
+// 这样可以用`Reflux`管理全部的`state`
+// 在这里面改变`state`是不会生效的
+// 否则，还是使用`React.Component`
+// 更多用法请看PageDemo.js文件
+class PageHome extends Component {
+
+  render() {
+    const renderCell = cellData => (<span>{cellData}</span>);
+    const tableProps = {
+      width: 900,
+      showSearch: true,
+      fetchUrl: URLS.getSomeInfo,
+      beforeFetch(data) {
+        const { currentPage, pageSize, searchTxt } = data;
+        return {
+          currentPage,
+          pageSize,
+          workNo: parseInt(searchTxt, 10) || 0,
+        };
+      },
+      searchBarPlaceholder: '请输入员工工号',
+      emptyText: '没有查到符合条件的信息',
+      jsxcolumns: [
+        { dataKey: 'workNo', title: '工号', width: 300, render: renderCell },
+        { dataKey: 'name', title: '姓名', width: 300, render: renderCell },
+        { dataKey: 'nickName', title: '花名', width: 300, render: renderCell },
+      ],
+    };
+    return (
+      <div className="page-home">
+        <Table {...tableProps} />
+      </div>
+    );
+  }
+}
+
+
+export default withRouter(PageHome);
+
+
